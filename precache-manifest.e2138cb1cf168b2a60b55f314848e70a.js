55c3f68568e1da461bc22218da3a71376ebeebd5
Ivan <cellarrainbow@gmail.com>
2019-03-28 14:43:52 +0300

    Updates

---
+++ precache-manifest.e2138cb1cf168b2a60b55f314848e70a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a15623327405801734e",
+    "url": "/crudapp/static/js/runtime~main.35631f4c.js"
+  },
+  {
+    "revision": "ea1f3fc8eafec6666953",
+    "url": "/crudapp/static/js/main.a7d5e2fb.chunk.js"
+  },
+  {
+    "revision": "cf11de93aed8fa408fc3",
+    "url": "/crudapp/static/js/2.d0769803.chunk.js"
+  },
+  {
+    "revision": "ea1f3fc8eafec6666953",
+    "url": "/crudapp/static/css/main.3025b6ac.chunk.css"
+  },
+  {
+    "revision": "0c0687e5ce6f54692434427f9149a58e",
+    "url": "/crudapp/index.html"
+  }
+];
